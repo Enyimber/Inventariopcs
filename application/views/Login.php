31d36ebae735cb71931f78b09f9f59e39d333ca8
Enyimber Aguilar <enyimber1999@gmail.com>
2023-06-16 22:44:15 +0000

    codeiniter inventario

---
+++ application/views/Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="icon" href="https://codeigniter.com/favicons/favicon.png">
+    <link rel="stylesheet" href= "<?= base_url()?>/plantilla/stilos.css">
+</head>
+<body>
+    <div class="container">
+        <form action="">
+            <div class="">
+                <input type="text" placeholder="usuario">
+            </div>
+            <div class="">
+            <input type="password" placeholder="contraceña">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
